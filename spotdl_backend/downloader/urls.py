26bc5ec8005e565f5344436f0aa11376d6a79cc7
jy <92442809+jiayng01@users.noreply.github.com>
2025-02-26 22:40:52 +0800

    Clear

--- spotdl_backend/downloader/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("download/", views.download_song, name="download_song"),
-    path(
-        "media/<str:file_name>", views.download_file, name="download_file"
-    ),  # TODO: why is this a path? use DB instead
-]
