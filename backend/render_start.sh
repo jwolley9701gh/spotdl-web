d7df7aea6dd6e8018d2fa1c15908a46afb743030
jia ying <92442809+jiayng01@users.noreply.github.com>
2025-05-03 00:36:56 +0800

    Update start.sh

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "FFmpeg path: $(which ffmpeg)"
+
+# Normal startup commands
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn backend.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
