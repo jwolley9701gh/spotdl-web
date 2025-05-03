17f08db5942902b7c4168eec855dd240096f2438
jia ying <92442809+jiayng01@users.noreply.github.com>
2025-05-04 04:05:59 +0800

    Edit font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   )
 }
